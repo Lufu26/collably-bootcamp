e98be6e6277077c7600d687be17d3d4014f71b87
george <malalelufu26@gmail.com>
2025-08-08 08:55:28 -0400

    feat: Add initial JavaScript file and practice variables.

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("hello from script.js");
+
+const name = "George";
+const age = 23;
+const isStudent = true;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+let info = `My name is ${name} & I am ${age} years  old`;
+
+console.log(info);
+
+const favNum1 = 10;
+const favNum2 = 30;
+let sum = favNum1 + favNum2;
+let product = favNum1 * favNum2;
+console.log(sum);
+console.log(product);
+
+
+
+
+
+
